8cc455ac3f90fe74052bcf392aa1cf647125da63
YYH <yunhanya@usc.edu>
2018-03-04 02:30:35 -0800

    code

---
+++ app.js
@@ -0,0 +1,29 @@
+/*var builder = require('botbuilder');
+
+var connector = new builder.ConsoleConnector().listen();
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+}); */
+
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: 'ef92f9e7-6ca8-4ffc-80e1-969ffb61124b',
+    appPassword: '#Pd0!PFfx[Ov+iT{'
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
