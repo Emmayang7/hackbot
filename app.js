05dae7cac80c28b091244e0d16984ede85d725b9
Jingyun Yang <jingyuny@usc.edu>
2018-03-04 07:20:56 -0800

    More edits.

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: 'ef92f9e7-6ca8-4ffc-80e1-969ffb61124b',
-    appPassword: '#Pd0!PFfx[Ov+iT{'
+    appId: 'ef92f9e7-6ca8-4ffc-80e1-969ffb61124b',//process.env.MicrosoftAppId,
+    appPassword: '#Pd0!PFfx[Ov+iT{',//process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata
 });
 
 // Listen for messages from users 
@@ -22,45 +23,50 @@ var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
 // Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector, function (session, args) {
-    session.send('default', session.message.text);
-});
+var bot = new builder.UniversalBot(connector);
 bot.set('storage', tableStorage);
+
 var luisAppId = process.env.LuisAppId;
 var luisAPIKey = process.env.LuisAPIKey;
-var luisAPIHostName = process.env.LuisAPIHostName;
+var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
 
-const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v2.0/apps/' + luisAppId + '?subscription-key=' + luisAPIKey;
+const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
 
 // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 .matches('AttendHackathon', (session) => {
+	console.log("AttendHackathon");
     const possibleResponses = ["Nice to see that you love hackathons. Could you describe one of the projects you like the best?"];
     const rand = Math.floor(Math.random() * possibleResponses);
     session.send(possibleResponses[rand]);
 })
 .matches('Collaborate', (session) => {
+	console.log("Collaborate");
 	const possibleResponses = ["Sounds good. So in terms of collaboration, have you been faced with a difficult situation in a team? How did you deal with that?"];
     const rand = Math.floor(Math.random() * possibleResponses);
     session.send(possibleResponses[rand]);
 })
 .matches('Intern', (session) => {
+	console.log("Intern");
 	const possibleResponses = ["Can you tell me more about the intern project you just mentioned? What did you enjoy most about it?"];
     const rand = Math.floor(Math.random() * possibleResponses);
     session.send(possibleResponses[rand]);
 })
 .matches('Leadership', (session) => {
+	console.log("Leadership");
 	const possibleResponses = ["I see that you have a decent amount of leadership experience. Do you like to work in a team as a leader or as a member? Why?"];
     const rand = Math.floor(Math.random() * possibleResponses);
     session.send(possibleResponses[rand]);
 })
 .matches('MachineLearning', (session) => {
+	console.log("MachineLearning");
 	const possibleResponses = ["So you know about machine learning? What are some of the projects you have completed using machine learning?"];
     const rand = Math.floor(Math.random() * possibleResponses);
     session.send(possibleResponses[rand]);
 })
 .onDefault((session) => {
+	console.log("Default");
 	const possibleResponses = ["Nice!", "Good.", "Good job."];
     const rand = Math.floor(Math.random() * possibleResponses);
     session.send(possibleResponses[rand]);
